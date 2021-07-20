8c0273440d43a1ec382917f4848165b14a0e731b
hectordev <hsoto.doer@gmail.com>
2021-07-20 03:48:53 -0500

    Esta es otra forma de usar while.

--- bucles.py
+++ bucles.py
@@ -1,11 +1,27 @@
+# def run():
+#     exponente = 1
+#     while exponente < 1001:
+#         print("2 elevado a " + str(exponente) + " " + "es igual a " + str(2 ** exponente))
+#         if exponente == 1000:
+#             break
+#         exponente += 1
+    
+    
+# if __name__ == "__main__":
+#     run()
+
 def run():
-    exponente = 1
-    while exponente < 1001:
-        print("2 elevado a " + str(exponente) + " " + "es igual a " + str(2 ** exponente))
-        if exponente == 1000:
-            break
-        exponente += 1
-    
-    
+    LIMITE = 1000000
+
+    exp = 0
+    potencia = 2 ** exp
+    while potencia < LIMITE:
+        print("2 elevado a " + str(exp) + " " + "es igual a " + str(potencia))
+        exp = exp + 1
+        potencia = 2 ** exp
+
+
+
+
 if __name__ == "__main__":
     run()
