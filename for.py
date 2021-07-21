db8b3e021c457eaaab6cf8fb326a62c7344e876f
hectordev <hsoto.doer@gmail.com>
2021-07-21 03:25:18 -0500

    Estoy disfrutando del ciclo for

---
+++ for.py
@@ -0,0 +1,21 @@
+# contador = 1
+# while contador < 1001:
+#     print(contador)
+#     contador += 1
+
+
+# a = list(range(1000))
+# print(a)
+
+
+# for contador in range(1, 1001):
+#     print(contador)
+
+for a in range(21):
+    print(2 ** a)
+
+
+
+
+
+
