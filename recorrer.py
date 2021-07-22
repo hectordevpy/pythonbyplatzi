127d43007de914c563e29b68798b88f391d00bfb
hectordev <hsoto.doer@gmail.com>
2021-07-22 01:57:29 -0500

    Recorriendo un string con for

---
+++ recorrer.py
@@ -0,0 +1,15 @@
+def run():
+    # nombre = input("Escribe tu nombre: ")
+    # for letra in nombre:
+    #     print(letra)
+
+    frase = input("Escribe una frase: ")
+    for palabra in frase:
+        print(palabra.upper())
+
+
+
+
+
+if __name__ == "__main__":
+    run()
